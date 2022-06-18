f4f90407208f9f101a54287b3a077f707637d10a
alam <alamdream@gmail.com>
2022-06-17 18:45:55 -0600

    added readme file

--- src/test/java/mortgage_calculator/BaseClass.java
+++ src/test/java/mortgage_calculator/BaseClass.java
@@ -1,2 +1,37 @@
-package mortgage_calculator;public class BaseClass {
+package mortgage_calculator;
+
+import command_providers.ActOn;
+import org.apache.logging.log4j.ThreadContext;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import utilities.DriverFactory;
+import utilities.ReadConfigFiles;
+
+import java.util.logging.Logger;
+
+public class BaseClass {
+    public WebDriver driver;
+    Logger LOGGER = Logger.getLogger(this.getClass().getName());
+    String testCaseName = String.format("---------Test: %s-----------", this.getClass().getName());
+    String endTestCase = String.format("--------Test End: %s---------",this.getClass().getName());
+
+    @BeforeClass
+    public void addThread(){
+        ThreadContext.put("threadName",this.getClass().getName());
+        driver = DriverFactory.getInstance().getDriver();
+    }
+    @BeforeMethod
+    public void browserInitialization(){
+        String browserUrl = ReadConfigFiles.getPropertyValues("Url");
+        LOGGER.info(testCaseName);
+        LOGGER.info("Opening the URL: " + browserUrl);
+        ActOn.browser(driver).openBrowser(browserUrl);
+    }
+    @AfterMethod
+    public void closeBrowser(){
+        DriverFactory.getInstance().removeDriver();
+        LOGGER.info(endTestCase);
+    }
 }
--- src/test/java/utilities/DriverFactory.java
+++ src/test/java/utilities/DriverFactory.java
@@ -1,2 +1,31 @@
-package utilities;public class DriverFactory {
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverFactory {
+    //do not allow to initialize this class from outside
+    private DriverFactory(){
+        // do - nothing-- empty constructor
+    }
+    private static final DriverFactory instance = new DriverFactory();
+    public static DriverFactory getInstance(){
+        return instance;
+    }
+    // thread local driver object for WebDriver
+    ThreadLocal<WebDriver> driver = ThreadLocal.withInitial(()->{
+        WebDriverManager.chromedriver().setup();
+        //Can be replaced with other browser drivers
+        return new ChromeDriver();
+    });
+    // call this method to get the driver object and launch the browser
+    public WebDriver getDriver(){
+        return driver.get();
+    }
+    //Quits the driver and closes the browser
+    public void removeDriver(){
+    driver.get().quit();
+    driver.remove();
+    }
 }
